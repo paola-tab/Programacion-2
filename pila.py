a73140c581ef1b9100fd9cf34909316683c02f55
paola <tabarespaola0@gmail.com>
2019-08-22 21:31:39 -0300

    22/08/2019

--- pila.py
+++ pila.py
@@ -28,29 +28,97 @@ class Pila:
        while not(self.estavacia()):
             self.extraer()
 
-#revertir una cadena de palabras
+'''revertir una cadena de palabras'''
 def revertir(a):
-    p = Pila()    
-    aux=Pila()
-    for x in range(len(a)):
-        p.incluir(a[x])
-    while not(p.estavacia()):   
-        if p.inspeccionar() != ' ':
-            aux.incluir(p.extraer())
+    p = Pila()
+    for x in a.split(" "):
+        p.incluir(x)
+    l=[]
+    while not p.estavacia():
+        l = l + [p.extraer()]
+    return " ".join(l)
+#print(revertir("Mi diario Python"))
+
+'''misma cantidad de 0 que de 1'''
+def cantidad(m):
+    p=Pila()
+    l=Pila()
+    for x in range(len(m)):
+        if m[x]=='0':
+            p.incluir(0)
+        elif m[x]=='1':
+            l.incluir(1)
+    if p.tamano() == l.tamano():
+        return True
+    else:
+        return False
+#print(cantidad("1010"))
+
+'''parentesis balanceado'''
+def parentesis(c):
+    resp=True
+    p=Pila()
+    for i in range(len(c)):
+        if c[i] == '(':
+            p.incluir(c[i])
+        if c[i] == ')':
+            if p.estavacia():
+                resp=False
+            if not p.estavacia():
+                p.extraer()
+    if p.tamano() != 0:
+        resp=False
+    return resp
+#print(parentesis('(())('))
+
+'''Validez'''
+def validez(v):
+    p=Pila()
+    for i in range (len(v)):
+        if v[i] == '(' or v[i] == '[' or v[i] == '{':
+            p.incluir (v[i])
+        if not p.estavacia():
+            if v[i] == ')':
+                if p.inspeccionar() == '(':
+                    p.extraer() 
+                else:
+                    return False
+            elif v[i] == ']':
+                if p.inspeccionar() == '[':
+                    p.extraer()
+                else:
+                    return False
+            elif v[i] == '}':
+                if p.inspeccionar()== '{':
+                    p.extraer()
+                else:
+                    return False
+        if p.tamano() == 0:
+            return True
         else:
-            aux.dar_vuelta() 
-            aux.imprimir()
+            return False
+#print (validez("([["))
+        
+'''capicua'''
+def capicua(num):
+    p=Pila()
+    for i in range (len(num)):
+        p.incluir(num[i])
+    j=0
+    #print(p.inspeccionar())
+    p.imprimir()
+    while not p.estavacia():
+        if p.inspeccionar() == num[j]:
+            j=+1
             p.extraer()
-            aux.vaciar()
-    aux.dar_vuelta() 
-    aux.imprimir()
-    
-revertir("Mi diario Python")
-
-        
-        
-
-        
+            #return True
+        else:
+            print(j)
+            print(p.tamano())
+            print(p.inspeccionar())
+            return False
+    return True
+print (capicua("neuquen"))
 
 #p=Pila()
 #(p.incluir(1))
